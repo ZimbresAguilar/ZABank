b3d92f3f8ae56079d827fc77c18d9d3d5a110aa0
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-06-20 09:16:25 -0300

    Começando ZABank

---
+++ telas/TelaLogin.java
@@ -0,0 +1,14 @@
+package telas;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TelaLogin extends Application{
+    @Override
+    public void start(Stage primaryStage) {}
+}
