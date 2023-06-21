5e69cadddfc1d1a3cf8ac032ca02c027556e014d
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-06-21 09:36:55 -0300

    Landing Page "Finalizada"

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+//  import javafx.geometry.Pos;
+//  import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+//  import javafx.scene.image.Image;
+//  import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+//  import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import telas.TelaCriarConta;
@@ -13,21 +20,47 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Banco SAPPOO");
-        
+    public void start(Stage primaryStage) {                 //Os comentários seguidos de TAB é os códigos para inserir a imagem... está "funcionando" mas não da pra ver os botões com ele funcionando, então teria que arrumar
+        primaryStage.setTitle("ZA Bank");
+
         // Criação dos elementos da tela inicial
         Button btnAcessarConta = new Button("Acessar Conta");
         Button btnCriarConta = new Button("Criar Conta");
         Button btnSair = new Button("Sair");
-        VBox root = new VBox(btnAcessarConta, btnCriarConta, btnSair);
+
+        //  // Carrega a imagem do arquivo
+        //  Image image = new Image("img/zaLogo.jpg");
+        //  // Cria um ImageView e define a imagem nele
+        //  ImageView imageView = new ImageView(image);
+        //  imageView.setFitWidth(700); // Largura da janela
+        //  imageView.setFitHeight(250); // Altura da janela
+
+        // Criação do GridPane, AnchorPane e StackPane
+        GridPane gridPane = new GridPane();
+        AnchorPane anchorPane = new AnchorPane();
+        //  StackPane stackPane = new StackPane();
+
+        // Espaçamento do GridPane, AnchorPane e StackPane
+        gridPane.setPadding(new Insets(10));
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+        AnchorPane.setLeftAnchor(gridPane, 225.0);
+        AnchorPane.setBottomAnchor(gridPane, 30.0);
+        //  StackPane.setAlignment(imageView, Pos.TOP_CENTER);
+
+        // Adicionando elementos ao GridPane, AnchorPane e StackPane
+        gridPane.add(btnAcessarConta, 0, 10);
+        gridPane.add(btnCriarConta, 1, 10);
+        gridPane.add(btnSair, 2, 10);
+        anchorPane.getChildren().add(gridPane);
+        //  stackPane.getChildren().add(imageView);
 
         // Ação do botão Acessar Conta
         btnAcessarConta.setOnAction(event -> {
-            //Fecha janela atual
+            // Fecha janela atual
             primaryStage.close();
 
-            //Cria uma instância da tela pra acessar a conta e a exibe
+            // Cria uma instância da tela pra acessar a conta e a exibe
             TelaLogin telaAcessarConta = new TelaLogin();
             telaAcessarConta.start(new Stage());
         });
@@ -46,8 +79,10 @@ public class Main extends Application {
         btnSair.setOnAction(event -> primaryStage.close());
 
         // Configuração da cena e exibição da janela
-        Scene scene = new Scene(root, 300, 200);
+        Scene scene = new Scene(anchorPane, 700, 400);
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.sizeToScene();
     }
 }
